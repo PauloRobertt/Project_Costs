dbe971054ad936eba03c1698f438eccfdc907502
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-11-06 00:24:56 -0300

    feat: pagina novo projeto

--- Costs/src/main/java/br/com/costs/Costs/View/costs_view/src/components/pages/NewProject.js
+++ Costs/src/main/java/br/com/costs/Costs/View/costs_view/src/components/pages/NewProject.js
@@ -1,7 +1,12 @@
+import ProjectForm from '../project/ProjectForm';
+import styles from './NewProject.module.css';
+
 export default function NewProject(){
     return(
-        <div>
-            <p>New Project</p>
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm btnText="Criar projeto"/>
         </div>
     )
 };
