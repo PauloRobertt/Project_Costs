739a629ed88e2f18f49bc314985eb89725caac98
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-11-08 00:37:56 -0300

    chore: delete da tabela categoria

--- Back-End/src/main/java/br/com/costs/Costs/Repository/CategoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.costs.Costs.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.costs.Costs.Model.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {}
