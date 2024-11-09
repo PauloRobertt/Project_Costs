8811bbb33201e98a99acc15220447dd29eca357c
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-11-09 17:03:56 -0300

    feat: layout da pagina de projetos

--- Front-End/costs_view/src/components/pages/Projects.js
+++ Front-End/costs_view/src/components/pages/Projects.js
@@ -1,8 +1,15 @@
 import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import styles from './Projects.module.css';
+import Container from '../layout/Container.js'
 
 import Message from "../layout/Message";
+import LinkButton from "../layout/LinkButton.js";
+import ProjectCard from "../project/ProjectCard.js";
 
 export default function Projects(){
+    const [projects, setProjects] = useState([]);
 
     const location = useLocation();
     let message = '';
@@ -11,10 +18,39 @@ export default function Projects(){
         message = location.state.message;
     }
 
+    useEffect(()=>{
+        fetch("http://localhost:8080/projeto",{
+            method:'GET',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+        })
+        .then((resp)=> resp.json())
+        .then((data)=> {
+            setProjects(data);
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+    },[])
+
     return(
-        <div>
-            <h1>Meus projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Novo Projeto"/>
+            </div>
             {message && <Message type="sucess" msg={message}/>}
+            <Container customClass="start">
+                {projects.length > 0 && 
+                    projects.map((project) => <ProjectCard
+                    id={project.id}
+                    nome={project.nome}
+                    orcamento={project.orcamento}
+                    categoria={project.categoria}
+                    key={project.id}
+                />)}
+            </Container>
         </div>
     );
 }
