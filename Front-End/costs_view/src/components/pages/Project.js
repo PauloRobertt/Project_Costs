95540d791c979a049f9e725cecf9716a14de9547
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-11-21 20:30:45 -0300

    feat(front-end): exibir lista de serviços associados ao projeto
    - Implementado mapeamento e exibição de serviços no componente Project.js.
    - Criado componente ServicoCard para exibir informações detalhadas de cada serviço.
    - Ajustado carregamento inicial para buscar serviços associados ao projeto.
    
    Resolves: #11

--- Front-End/costs_view/src/components/pages/Project.js
+++ Front-End/costs_view/src/components/pages/Project.js
@@ -5,16 +5,31 @@ import { useEffect, useState } from "react";
 import Loading from "../layout/Loading.js";
 import Container from "../layout/Container.js";
 import ProjectForm from "../project/ProjectForm.js";
+<<<<<<< Updated upstream
 import Message from '../layout/Message.js';
+=======
+import ServiceForm from "../service/ServicoForm.js";
+import Message from '../layout/Message.js';
+import ServicoCard from "../service/ServicoCard.js";
+>>>>>>> Stashed changes
 
 export default function Project() {
   const { id } = useParams();
 
   const [project, setProject] = useState([]);
+<<<<<<< Updated upstream
   const [showProject, setShowProject] = useState(false);
   const [typeMessage, setTypeMessage] = useState();
   const [txtMessage, setTxtMessage] = useState();
   
+=======
+  const [service, setService] = useState([]);
+  const [showProject, setShowProject] = useState(false);
+  const [showServico, setShowServico] = useState(false);
+  const [typeMessage, setTypeMessage] = useState();
+  const [txtMessage, setTxtMessage] = useState();
+
+>>>>>>> Stashed changes
   useEffect(() => {
     fetch(`http://localhost:8080/projeto/${id}`, {
       method: "GET",
@@ -31,7 +46,35 @@ export default function Project() {
       });
   }, [id]);
 
+<<<<<<< Updated upstream
   function editPost(project) {
+=======
+  useEffect(() => {
+    fetch("http://localhost:8080/servico", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setService(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }, [])
+
+  function editPost(project) {
+    setTxtMessage('');
+
+    if (project.orcamento < project.totalUtilizado) {
+      setTypeMessage('error');
+      setTxtMessage('Custo muito elevado!');
+      return false;
+    }
+
+>>>>>>> Stashed changes
     fetch(`http://localhost:8080/projeto/${id}`, {
       method: "PUT",
       headers: {
@@ -40,7 +83,11 @@ export default function Project() {
       body: JSON.stringify(project),
     })
       .then((resp) => resp.json())
+<<<<<<< Updated upstream
       .then((data)=>{
+=======
+      .then((data) => {
+>>>>>>> Stashed changes
         setProject(data);
         setShowProject(!showProject);
         setTypeMessage("sucess");
@@ -53,11 +100,47 @@ export default function Project() {
       });
   }
 
+<<<<<<< Updated upstream
+=======
+  function createService(service) {
+    setTxtMessage('');
+
+    if (project.orcamento < project.totalUtilizado) {
+      setTypeMessage('error');
+      setTxtMessage('Custo muito elevado!');
+      return false;
+    }
+
+    fetch("http://localhost:8080/servico", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(service)
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setTypeMessage("sucess");
+        setTxtMessage("Projeto Editado com Sucesso!");
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+>>>>>>> Stashed changes
 
   function toggleProjectForm() {
     setShowProject(!showProject);
   }
 
+<<<<<<< Updated upstream
+=======
+  function toggleServicoForm() {
+    setShowServico(!showServico);
+  }
+
+>>>>>>> Stashed changes
   return (
     <>
       {!project.nome ? (
@@ -65,7 +148,11 @@ export default function Project() {
       ) : (
         <div className={styles.project_details}>
           <Container customClass="column">
+<<<<<<< Updated upstream
             <Message type={typeMessage} msg={txtMessage}/>
+=======
+            <Message type={typeMessage} msg={txtMessage} />
+>>>>>>> Stashed changes
             <div className={styles.details_container}>
               <h1>Projeto: {project.nome}</h1>
               <button className={styles.btn} onClick={toggleProjectForm}>
@@ -96,6 +183,37 @@ export default function Project() {
                 </div>
               )}
             </div>
+<<<<<<< Updated upstream
+=======
+            <div className={styles.service_form_container}>
+              <h2>Adicione um serviço:</h2>
+              <button className={styles.btn} onClick={toggleServicoForm}>
+                {showServico ? "Fechar" : "Adicionar Serviço"}
+              </button>
+              {!showServico ?
+                <div className={styles.project_info}></div> :
+                <div className={styles.project_info}>
+                  <ServiceForm
+                    btnText={"Adicionar Serviço"}
+                    handleSubmit={createService}
+                    projectData={project}
+                  />
+                </div>}
+            </div>
+            <h2>Serviços:</h2>
+            <Container customClass='start'>
+              {service.length > 0 &&
+                service.map((servico) =>
+                  <ServicoCard
+                    id={servico.id}
+                    nome={servico.nome}
+                    descricao={servico.descricao}
+                    custo={servico.custo}
+                    key={servico.id}
+                  />
+                )}
+            </Container>
+>>>>>>> Stashed changes
           </Container>
         </div>
       )}
---
+++ Front-End/costs_view/src/components/service/ServicoCard.js
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+import styles from './ServicoCard.module.css';
+
+import { BsPencil, BsFillTrashFill } from 'react-icons/bs';
+
+export default function ServicoCard({id, nome, descricao, custo, handleRemove}){
+
+    function remove(e){
+        e.preventDefault();
+        handleRemove(id);
+    }
+
+    return(
+        <div className={styles.project_card}>
+            <h4>{nome}</h4>
+            <p>
+                <span>custo:</span> R${custo}
+            </p>
+            <p className={styles.category_text}>
+                <span className={`${styles[descricao.toLowerCase()]}`}></span> {descricao}
+            </p>
+            <div className={styles.project_card_actions}>
+                <Link to={`/project/${id}`}>
+                    <BsPencil/>Editar
+                </Link>
+                <button onClick={remove}>
+                    <BsFillTrashFill/>Excluir
+                </button>
+            </div>
+        </div>
+    );
+}
