a33d07edfbdf9a73998ae65f29d58f94eb399926
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-11-21 20:30:23 -0300

    feat(front-end): criar formulário para adicionar serviços
    - Criado componente ServiceForm para adicionar novos serviços.
    - Integrado formulário com a API para realizar requisições POST.
    - Adicionados estilos personalizados em Input.js e Input.module.css.
    
    Resolves: #11

--- Front-End/costs_view/src/components/form/Input.js
+++ Front-End/costs_view/src/components/form/Input.js
@@ -1,9 +1,9 @@
 import styles from './Input.module.css';
 
-export default function Input({type, text, name, placeholder, handleOnChange, value, maxLength, minLength, min}){
+export default function Input({type, text, name, placeholder, handleOnChange, value, maxLength, minLength, min,}){
     return(
         <div className={styles.form_control}>
-            <label htmlFor={name}>{text}:</label>
+            <label htmlFor={name}>{text}</label>
             <input 
                 type={type} 
                 name={name} 
---
+++ Front-End/costs_view/src/components/service/ServicoForm.js
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+
+import Input from '../form/Input.js';
+import SubmitButton from '../form/SubmitButton.js';
+import styles from './ServicoForm.module.css';
+
+export default function ServiceForm({ btnText, handleSubmit, projectData }) {
+    const [project, setProject] = useState(projectData);
+    const [servico, setServico] = useState({
+        nome: '',
+        descricao: '',
+        custo: '',
+        projetoID: project.id || ''
+    });
+
+
+    const submit = (e) => {
+        e.preventDefault();
+        console.log("Objeto Serviço: ", servico);
+        handleSubmit(servico);
+    }
+
+    function handleChange(e) {
+        setServico({ ...servico, [e.target.name]: e.target.value })
+    }
+
+    return (
+        <form onSubmit={submit} className={styles.form}>
+            <Input
+                type="text"
+                text="Nome do Serviço"
+                name="nome"
+                value={servico.nome}
+                placeholder="Insira o nome do serviço"
+                handleOnChange={handleChange}
+                maxLength={100}
+                minLength={3}
+            />
+
+            <Input
+                type="text"
+                text="Descrição do Serviço"
+                name="descricao"
+                value={servico.descricao}
+                placeholder="Insira a descrição do serviço"
+                handleOnChange={handleChange}
+                maxLength={50}
+                minLength={3}
+            />
+
+            <Input
+                type="number"
+                text="Custo do Serviço"
+                name="custo"
+                value={servico.custo}
+                handleOnChange={handleChange}
+                placeholder="Insira o custo do serviço"
+                min={0}
+            />
+
+            <Input
+                type="hidden"
+                name="projetoID"
+                value={project.id}
+            />
+
+            <SubmitButton text={btnText} />
+        </form>
+    );
+}
